68231893b73b8d83c8b05679d819956871cd90a5
Rom Kajaria <romilkajaria@gmail.com>
2023-11-30 02:11:03 +0800

    lots

--- TheBigDay/Program.cs
+++ TheBigDay/Program.cs
@@ -1,6 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 using System.Reflection;
 using TheBigDay.DBContext;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Identity.Web;
 
 public static class Program
 {
@@ -8,51 +11,36 @@ public static class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
+        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme);
+
         // Add services to the container.
 
         builder.Services.AddControllers();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen(options =>
-        {
-            //options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "API", Version = "v1" });
-
-            //options.AddSecurityDefinition("oauth2", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
-            //{
-            //    Type = Microsoft.OpenApi.Models.SecuritySchemeType.OAuth2,
-            //    Flows = new Microsoft.OpenApi.Models.OpenApiOAuthFlows
-            //    {
-            //        ClientCredentials = new Microsoft.OpenApi.Models.OpenApiOAuthFlow
-            //        {
-            //            TokenUrl = new Uri(""),
-            //            Scopes = new Dictionary<string, string>
-            //            {
-            //                {"api", "API" }
-            //            }
-            //        }
-            //    }
-            //});
-        });
+        builder.Services.AddSwaggerGen();
 
         builder.Configuration.AddEnvironmentVariables().AddUserSecrets(Assembly.GetExecutingAssembly(), true);
 
-        string connectionString = builder.Configuration.GetSection("TBDContext").Get<TBDContext>().TBDDatabaseConnectionString;
-        builder.Services.AddDbContext<DatabaseContext>(options => options.UseSqlServer(connectionString));
+        //string connectionString = builder.Configuration.GetSection("TBDContext").Get<TBDContext>().TBDDatabaseConnectionString;
+        builder.Services.AddDbContext<DatabaseContext>(options => options.UseSqlServer("Data Source=(localdb)\\ProjectsV13;Initial Catalog=TheBigDay;Integrated Security=True"));
 
         var app = builder.Build();
 
-
+        app.UseSwagger();
+        app.UseSwaggerUI();
 
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
-            app.UseSwagger();
-            app.UseSwaggerUI();
+
         }
 
 
 
         app.UseHttpsRedirection();
+
+        app.UseAuthentication();
 
         app.UseAuthorization();
 
@@ -63,7 +51,7 @@ public static class Program
 
     public class TBDContext
     {
-        public string TBDDatabaseConnectionString { get; set; }
+        public string? TBDDatabaseConnectionString { get; set; }
     }
 }
 
