2fd1ab6c5dc86a39c85cb5d958f70925c2809d2b
Rom <Romilkajaria@gmail.com>
2024-01-20 01:19:38 +0800

    show data on store + dist

--- TheBigDay.Web/projects/consumer/src/app/pages/vendor-store/vendor-store.component.ts
+++ TheBigDay.Web/projects/consumer/src/app/pages/vendor-store/vendor-store.component.ts
@@ -11,18 +11,20 @@ import {
 } from "../../../../../common/src/lib/common-rest-services/vendors/common-vendor-service.service";
 import {Product} from "../../../../../common/src/lib/common-rest-models/product";
 import {Service} from "../../../../../common/src/lib/common-rest-models/service";
+import {IDashboardCard} from "../../../../../common/src/lib/components/uikit/dashboard-card/dashboard-card.component";
+import {TBDItem} from "../../../../../common/src/lib/common-rest-models/item";
+import {Vendor} from "../../../../../common/src/lib/common-rest-models/vendor";
 
 @Component({
     selector: 'app-vendor-store',
     templateUrl: './vendor-store.component.html',
     styleUrls: ['./vendor-store.component.scss'],
 })
-export class VendorStoreComponent implements OnInit{
+export class VendorStoreComponent implements OnInit {
+    public vendor?: Vendor;
     public productId?: string | null;
     public serviceId?: string | null;
     public vendorId?: string | null
-    public products: Product[] = [];
-    public services: Service[] = [];
 
     constructor(private route: ActivatedRoute,
                 private vendorService: CommonVendorService) {
@@ -39,10 +41,29 @@ export class VendorStoreComponent implements OnInit{
 
             if(this.vendorId) {
                 this.vendorService.getVendor(this.vendorId).subscribe((v) => {
-                    this.products = v.products;
-                    this.services = v.services;
+                    this.vendor = v
                 })
             }
         })
+    }
+
+    addCardWrapperProduct(item: Product): IDashboardCard<Product> {
+        return {
+            heading: item.name,
+            description: item.description,
+            maxWidth: '260px',
+            subheading: 'popular',
+            metadata: item
+        } as IDashboardCard<Product>;
+    }
+
+    addCardWrapperService(item: Service): IDashboardCard<Service> {
+        return {
+            heading: item.name,
+            description: item.description,
+            maxWidth: '260px',
+            subheading: 'popular',
+            metadata: item
+        } as IDashboardCard<Service>;
     }
 }
--- TheBigDay.Web/projects/consumer/src/app/pages/vendor-store/vendor-store.module.ts
+++ TheBigDay.Web/projects/consumer/src/app/pages/vendor-store/vendor-store.module.ts
@@ -3,13 +3,15 @@ import {CommonModule} from '@angular/common';
 
 import {VendorStoreRoutingModule} from './vendor-store-routing.module';
 import {VendorStoreComponent} from "./vendor-store.component";
+import {UIkitModule} from "../../../../../common/src/lib/components/uikit/uikit.module";
 
 
 @NgModule({
   declarations: [VendorStoreComponent],
   imports: [
     CommonModule,
-    VendorStoreRoutingModule
+    VendorStoreRoutingModule,
+      UIkitModule
   ]
 })
 export class VendorStoreModule { }
