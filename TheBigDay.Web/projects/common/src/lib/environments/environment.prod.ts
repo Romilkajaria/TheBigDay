81256577ec9076ffa399d9d23223bc32969e2d26
Rom <Romilkajaria@gmail.com>
2024-01-28 00:56:52 +0800

    auth working for vendor + dist

--- TheBigDay.Web/projects/common/src/lib/environments/environment.prod.ts
+++ TheBigDay.Web/projects/common/src/lib/environments/environment.prod.ts
@@ -1,4 +1,13 @@
+import {AuthConfig} from "@auth0/auth0-angular";
+
 export const environment = {
   production: true,
-  apiUrl: 'https://tbdvendors.azurewebsites.net/'
+  apiUrl: 'https://tbdvendors.azurewebsites.net/',
+    auth0: {
+        domain: 'dev-trnzolr3zks0yejt.au.auth0.com',
+        clientId: 'QwIuX9PpTzkEnYzJtflOq4HHopN7Fzkk',
+        authorizationParams: {
+            redirect_uri: "https://jolly-coast-0dc81a210.4.azurestaticapps.net",
+        }
+    } as AuthConfig,
 };
