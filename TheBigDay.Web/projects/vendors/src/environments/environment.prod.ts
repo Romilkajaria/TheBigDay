fa4eebfd766393b314597fc24a1b6e89bfb8cf1b
Rom <Romilkajaria@gmail.com>
2024-01-28 01:35:40 +0800

    update vendor env auth + dist

--- TheBigDay.Web/projects/vendors/src/environments/environment.prod.ts
+++ TheBigDay.Web/projects/vendors/src/environments/environment.prod.ts
@@ -4,8 +4,8 @@ export const environment = {
   production: true,
   apiUrl: 'https://tbdvendors.azurewebsites.net/',
     auth0: {
-        domain: 'dev-trnzolr3zks0yejt.au.auth0.com',
-        clientId: 'TSD8Tv41eutaRopTMJmUPjRbGbc0FlJo',
+        domain: 'prod-tbd-vendor.au.auth0.com',
+        clientId: 'rZmvcTWKQ3ZuyogLDpiWakysYi2vE0Rh',
         authorizationParams: {
             redirect_uri: "https://jolly-coast-0dc81a210.4.azurestaticapps.net",
         }
