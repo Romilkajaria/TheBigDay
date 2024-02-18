c79558c4cfe8bd7eb0415269c271bca1d326f6cf
Rom <Romilkajaria@gmail.com>
2024-02-19 00:38:54 +0800

    trying auth

---
+++ TheBigDay/Authentication/ResponseModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace TheBigDay.Authentication
+{
+    public class Response
+    {
+        public string Status { get; set; }
+        public string Message { get; set; }
+    }
+}
+
