d7b93c74a05cb85e13e88a32c128ec2cf922bc47
Rom Kajaria <romilkajaria@gmail.com>
2024-03-17 02:47:19 +0800

    add public endpionts for search

---
+++ TheBigDay/Controllers/PublicEndpointsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TheBigDay.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PublicEndpointsController : ControllerBase
+    {
+        // GET: api/<PublicEndpointsController>
+
+        // GET api/<PublicEndpointsController>/5
+        [HttpGet("products/{address}")]
+        public string GetProducts(string address)
+        {
+            return "value";
+        }
+
+        [HttpGet("services/{address}")]
+        public string GetServices(string address)
+        {
+            return "value";
+        }
+    }
+}
