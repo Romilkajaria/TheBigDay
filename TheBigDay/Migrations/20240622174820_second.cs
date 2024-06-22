11b9397438ccf12037a75c1c032c40dd78398d1c
Rom Kajaria <romilkajaria@gmail.com>
2024-06-23 04:30:01 +0800

    add some files

---
+++ TheBigDay/Migrations/20240622174820_second.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TheBigDay.Migrations
+{
+    /// <inheritdoc />
+    public partial class second : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ItemGategoryId",
+                table: "StoreItemCategory");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "ItemGategoryId",
+                table: "StoreItemCategory",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+    }
+}
