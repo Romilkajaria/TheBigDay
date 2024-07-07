39b007e0a9b857a6b14fb827b41824bc09d0b229
Rom <Romilkajaria@gmail.com>
2024-07-08 01:52:34 +0800

    add options

---
+++ TheBigDay/Migrations/20240707093150_FormFiledOptionsAndEventTypeDescription.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TheBigDay.Migrations
+{
+    /// <inheritdoc />
+    public partial class FormFiledOptionsAndEventTypeDescription : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Options",
+                table: "FormField",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "EventType",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Options",
+                table: "FormField");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "EventType");
+        }
+    }
+}
